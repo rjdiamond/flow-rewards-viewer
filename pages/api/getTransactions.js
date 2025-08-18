638e48fd463420e2031e6a3a8499f7c066e7deac
Diamond <52646393+rjdiamond@users.noreply.github.com>
2025-08-18 10:05:45 -0700

    1000 -> 100

--- pages/api/getTransactions.js
+++ pages/api/getTransactions.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'walletAddress is required' });
   }
 
-  const transactionsUrl = `https://store.flow.com/api/loyalty/transaction_entries?limit=1000&orderBy=createdAt&websiteId=8f6b91d5-985d-4da7-b253-68681eb4e4f0&walletAddress=${walletAddress}&organizationId=ebc23e35-e19a-4113-8951-dfff40192c5c&excludeDeletedCurrency=true`;
+  const transactionsUrl = `https://store.flow.com/api/loyalty/transaction_entries?limit=100&orderBy=createdAt&websiteId=8f6b91d5-985d-4da7-b253-68681eb4e4f0&walletAddress=${walletAddress}&organizationId=ebc23e35-e19a-4113-8951-dfff40192c5c&excludeDeletedCurrency=true`;
 
   const assetsUrl = `https://store.flow.com/api/minting/assets?limit=100&mintingContractIds=a33764e4-e993-4e64-aef3-6ea823afa991&websiteId=8f6b91d5-985d-4da7-b253-68681eb4e4f0&organizationId=ebc23e35-e19a-4113-8951-dfff40192c5c&isListed=true`;
 
